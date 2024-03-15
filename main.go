79b3a18e87eb46c2ebd131ede493110bc7a0cf26
Martin Bozhilov <marogo142005@gmail.com>
2024-03-15 12:53:24 +0200

    [ADD] Init Tables

--- main.go
+++ main.go
@@ -1,25 +1,27 @@
 package main
 
 import (
-	_ "github.com/joho/godotenv/autoload"
-	"google.golang.org/grpc"
 	"net"
 	"order-service/pkg/config"
 	"order-service/pkg/db"
 	"order-service/pkg/pb"
 	"order-service/pkg/service"
+
+	_ "github.com/joho/godotenv/autoload"
+	"google.golang.org/grpc"
 )
 
 func main() {
 	cfg := config.LoadConfig()
-	db := db.Init(cfg)
+	pgdb := db.Init(cfg)
+	db.InitTable(pgdb)
 
 	lis, err := net.Listen("tcp", "localhost:"+cfg.TCP_PORT)
 	if err != nil {
 		panic(err)
 	}
 
-	os := service.NewOrderService(db, cfg)
+	os := service.NewOrderService(pgdb, cfg)
 
 	grpcServer := grpc.NewServer()
 
--- pkg/db/db.go
+++ pkg/db/db.go
@@ -3,9 +3,10 @@ package db
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/lib/pq"
 	"order-service/pkg/config"
 	"strconv"
+
+	_ "github.com/lib/pq"
 )
 
 func Init(cfg *config.Config) *sql.DB {
@@ -30,4 +31,21 @@ func Init(cfg *config.Config) *sql.DB {
 
 	fmt.Println("Successfully connected!")
 	return db
+}
+
+func InitTable(db *sql.DB) {
+	_, err := db.Exec(`CREATE TABLE IF NOT EXISTS orders (
+		id UUID PRIMARY KEY,
+		user_id UUID NOT NULL,
+		listing_id UUID NOT NULL,
+		status VARCHAR(255) NOT NULL DEFAULT 'pending',
+		token_uri VARCHAR(255) NOT NULL
+		FOREIGN KEY (user_id) REFERENCES users(id),
+		FOREIGN KEY (listing_id) REFERENCES listings(id)
+	)`)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully created table!")
 }
